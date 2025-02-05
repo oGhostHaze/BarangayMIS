1fa965583d3e5c025bde7643b7866e4a60369858
Martin Joshua Garaza <martinjoshuagaraza@Martins-Mac-mini.local>
2025-02-05 21:58:00 +0800

    User Create Fix

--- resources/views/livewire/admin/update-user-form.blade.php
+++ resources/views/livewire/admin/update-user-form.blade.php
@@ -1,44 +1,41 @@
 <form action="{{ route('auth.admin.users.update', $user->id) }}" method="POST">
     @csrf
     <div class="form-row row">
-        <div class="mb-3 form-group col-md-6 col-sm-6">
-            <label for="name">User Name (Provided from HRIS)</label>
-            <input type="text" class="form-control disabled" wire:model.live='name'" name="name" readonly>
+        <div class="form-group mb-3 col-md-6 col-sm-6">
+            <label for="emp_name">User Full Name </label>
+            <input type="text" class="form-control disabled" wire:model='name' name="name">
         </div>
-        <div class="mb-3 form-group col-md-6 col-sm-6">
+        <div class="form-group mb-3 col-md-6 col-sm-6">
             <label for="username">Username</label>
             <input type="text" class="form-control" id="username" name="username" wire:model="username"
                 placeholder="Enter Username" required>
         </div>
-        <div class="mb-3 form-group col-md-6 col-sm-12">
+        <div class="form-group mb-3 col-md-6 col-sm-12">
             <label for="email">User Email</label>
             <input type="text" class="form-control" id="email" name="email" wire:model="email"
                 placeholder="Enter Email">
         </div>
+
+        <div class="form-group mb-3 col-md-6 col-sm-6" wire:ignore>
+            <label for="password">Assign Roles</label>
+            <select wire:model="userroles" name="userroles[]" id="userroles" class="form-control select2" multiple>
+                @foreach ($roles as $role)
+                    <option value="{{ $role->id }}">{{ $role->name }}</option>
+                @endforeach
+            </select>
+        </div>
     </div>
 
     <div class="form-row row">
-        <div class="mb-3 form-group col-md-6 col-sm-12">
+        <div class="form-group mb-3 col-md-6 col-sm-12">
             <label for="password">Password</label>
             <input type="password" class="form-control" id="password" name="password" wire:model="password"
                 placeholder="Enter Password">
         </div>
-        <div class="mb-3 form-group col-md-6 col-sm-12">
+        <div class="form-group mb-3 col-md-6 col-sm-12">
             <label for="password_confirmation">Confirm Password</label>
             <input type="password" class="form-control" id="password_confirmation" name="password_confirmation"
                 wire:model="password_confirmation" placeholder="Enter Password">
-        </div>
-    </div>
-
-    <div class="form-row row">
-        <div class="mb-3 form-group col-md-6 col-sm-6" wire:ignore>
-            <label for="password">Assign Roles</label>
-            <select wire:model="userroles" name="roles[]" id="userroles" class="form-control select2" multiple>
-                @foreach ($roles as $role)
-                    <option value="{{ $role->name }}" {{ $user->hasRole($role->name) ? 'selected' : '' }}>
-                        {{ $role->name }}</option>
-                @endforeach
-            </select>
         </div>
     </div>
 
