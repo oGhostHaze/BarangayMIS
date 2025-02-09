9189826a51fa5b24ade98bd3769fae00d58e607c
ChesyLyn <chesylynbaguiopimentel@gmail.com>
2025-02-09 22:17:15 +0800

    02-09-2025

--- resources/views/livewire/admin/create-user-form.blade.php
+++ resources/views/livewire/admin/create-user-form.blade.php
@@ -20,7 +20,7 @@
             <label for="password">Assign Roles</label>
             <select wire:model="userroles" name="userroles[]" id="userroles" class="form-control select2" multiple>
                 @foreach ($roles as $role)
-                    <option value="{{ $role->id }}">{{ $role->name }}</option>
+                    <option value="{{ $role->name }}">{{ $role->name }}</option>
                 @endforeach
             </select>
         </div>
